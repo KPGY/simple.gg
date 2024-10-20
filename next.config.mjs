7d8e39558be5234c27a7ded6b35eb3b8f0eb8cc7
KPGY <yuchw8@korea.ac.kr>
2024-10-21 02:50:59 +0900

    cors 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -10,16 +10,19 @@ const nextConfig = {
         source: '/api/:path*', // API 경로 지정
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // 모든 출처 허용
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://simple-gg.vercel.app',
+          }, // 특정 출처 허용
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          }, // 허용할 메서드
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT', // 허용할 메서드
+          },
           {
             key: 'Access-Control-Allow-Headers',
             value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          }, // 허용할 헤더
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version', // 허용할 헤더
+          },
         ],
       },
     ];
