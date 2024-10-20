0b62b77859a0e538d16af085a5172479181ef7a5
KPGY <yuchw8@korea.ac.kr>
2024-10-21 03:51:11 +0900

    cors 문제 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -6,22 +6,21 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // 모든 API 경로에 대해 CORS 헤더 추가
-        source: '/api/:path*', // API 경로 지정
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'simple-gg.vercel.app',
-          }, // 특정 출처 허용
+            value: 'https://simple-j8lj6ahah-kpgys-projects.vercel.app', // 특정 출처를 허용
+          },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT', // 허용할 메서드
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
             key: 'Access-Control-Allow-Headers',
             value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version', // 허용할 헤더
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
